6969b5db059efafb7a05edde068bb5bb2931fb22
zhiangsan <113005453+zhiangsan@users.noreply.github.com>
2022-09-07 13:48:47 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -52,7 +52,7 @@ export const config = {
       openUrl: "https://wangxinleo.cn"
     }, 
     {
-      name: "老婆2",
+      name: "大腿2",
       id: "",
       useTemplateId: "",
       province: "",
@@ -60,7 +60,7 @@ export const config = {
       openUrl: "https://wangxinleo.cn"
     }, 
     {
-      name: "老婆3", 
+      name: "大腿3", 
       id: "",
       useTemplateId: "",
       province: "",
@@ -106,7 +106,7 @@ export const config = {
    * 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
    */
   FESTIVALS: [
-    {"type": "生日", "name": "老婆", "year": "1996", "date": "09-02"},
+    {"type": "生日", "name": "老婆", "year": "1997", "date": "10-26"},
     {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
     {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
     {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
